0eb4a42df4ba6920f5924b08ff91eda97a2647f4
Rafaela Espanhol <rafaelacarolinaespanhol98@gmail.com>
2022-11-21 20:41:00 -0300

    formatos de login com bootstrap

--- index.php
+++ index.php
@@ -9,28 +9,41 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-    <main class="container-fluid">
+    <main class="container">
+        <div class="row justify-contet-center">
+            <div class="col-md-6 text-center mb-5">
+                <h2>Login</h2>
+</div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-md-12 col-lg-10">
+        <div class="row">
+            <div class="col-6">
+        <img class="img" src="foto.jpg" width="300px">
+</div>
+<div class="col-6 p-4 p-md-5"
+<div class="w-100>
+       <h3>Acesso Restrito</h3>
+</div>
 <form>
-    <div class="container-fluid">
-        <form>
-            <div class="mb-3 mt-5">
-                <input type="email" class="form-control"
-                placeholder="usuario/e-mail">
+    <div class="form-group mb-3">
+        <label class="label">Usuário/E-mail</label>
+        <input placeholder="usuário"
+
+          class="form-control>"
 </div>
-<div class="mb-3">
-    <input type="password" class="form-control"
-    placeholder="senha">
+    <div class="form-group mb-3">
+        <label class="label"Senha</label>
+        <input type="password"
+         class="form-control>
 </div>
-<div class="mb-3">
-    <button class="btn btn-primary">
-           Entrar 
-</button>
-<a href="cadastrar.php" class='btn btn-secondary">
-       Cadastrar
-      </a>
-  </div>
- </form>
- </main>
- 
+<div class="form-group">
+    <button class="btn btn-primary">Acessar</button>
+   <a href="cadastrar.php" class="btn btn-secondary">Registrar</a>
+</div>
+</form>
+<div><!-- termino formulario -->
+
+
 </body>
 </html>
