ae2d49114309aa15ab01ac9bf70ff6b1fb4584cc
Rafaela Espanhol <rafaelacarolinaespanhol98@gmail.com>
2022-11-21 22:26:05 -0300

    Cadastro cliente Bootstrap

---
+++ Cadastrar.php
@@ -0,0 +1,61 @@
+<form>
+<main class="container">
+        <div class="row justify-contet-center">
+            <div class="col-md-6 text-center mb-5">
+                <h2>Cadasto Cliente</h2>
+                </div>
+<div class="row justify-content-center">
+    <div class="col-md-12 col-lg-10">
+        <div class="row">
+            <div class="col-6">
+        <img class="img" src="foto2.jpg" width="400px">
+        <div class="form-group mb-3"> 
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+  <label for="fname">Nome:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+<label for="lname">Sobrenome:</label><br>
+  <input type="text" id="lname" name="lname">
+  <div class="form-group mb-3"> 
+  <label for="lname">E-mail</label><br>
+ <input type="text" id="lname" name="lname">
+
+ <div class="form-group mb-3"> 
+
+    <label for="Senha">Senha:</label>
+    <div class="form-group mb-3"> 
+    <input type="text" id="Senha" name="Senha">   
+
+<div>
+    <label for="pass"> Confirmação Senha :</label><div class="form-group mb-3"> 
+    <input type="password" id="pass" name="password"
+           minlength="8" required>
+           
+</div>
+
+<input type="submit" value="Sign in">
+
+<div class="form-group mb-3"> 
+<label for="start">Data de Nascimento:</label>
+<div class="form-group mb-3"> 
+<input type="date" id="start" name="trip-start"
+       value="2018-07-22"
+       min="2022-01-01" max="2022-12-31">
+
+       <label for="name">Endereço:</label>
+
+<input type="text" id="name" name="name" required
+       minlength="4" maxlength="8" size="10">         <label for="name">CEP:</label>
+
+<input type="text" id="name" name="name" required
+       minlength="4" maxlength="8" size="10">  
+      
+       <label for="name">Cidade:</label>  
+      
+<input type="text" id="name" name="name" required
+       minlength="4" maxlength="8" size="10">   <label for="name">UF:</label>  <input type="text" id="name" name="name" required
+   minlength="4" maxlength="8" size="10">  
+
+
